3b78bf1536f0d0d83ec4aeda6643c520efea94ee
ahinkle <ahinkle10@gmail.com>
2018-08-22 17:12:06 -0500

    Pagination Styling Init

--- resources/assets/js/components/Pagination.vue
+++ resources/assets/js/components/Pagination.vue
@@ -1,53 +1,27 @@
-<style lang="scss">
-.pagination {
-  justify-content: flex-end !important;
-  .page-stats {
-    align-items: center;
-    margin-right: 5px;
-  }
-  i {
-    color: #3273dc !important;
-  }
-}
-</style>
-
 <template>
-    <nav class="pagination" v-if="!client">
-        <span class="page-stats">{{pagination.from}} - {{pagination.to}} of {{pagination.total}}</span>
-        <a v-if="pagination.prevPageUrl" class="button is-small pagination-previous" @click="$emit('prev');">
-            Prev
-        </a>
-        <a class="button is-small pagination-previous" v-else :disabled="true">
-           Prev
-        </a>
-
-        <a v-if="pagination.nextPageUrl" class="button is-small pagination-next" @click="$emit('next');">
-            Next
-        </a>
-        <a class="button is-small pagination-next" v-else :disabled="true">
-            Next
-        </a>
-    </nav>
-
-    <nav class="pagination" v-else>
-        <span class="page-stats">
-            {{pagination.from}} - {{pagination.to}} of {{filtered.length}}
-            <span v-if="filtered.length < pagination.total">(filtered from {{pagination.total}} total entries)</span>
-        </span>
-        <a v-if="pagination.prevPage" class="button is-small pagination-previous" @click="$emit('prev');">
-            Prev
-        </a>
-        <a class="button is-small pagination-previous" v-else :disabled="true">
-           Prev
-        </a>
-
-        <a v-if="pagination.nextPage" class="button is-small pagination-next" @click="$emit('next');">
-            Next
-        </a>
-        <a class="button is-small pagination-next" v-else :disabled="true">
-            Next
-        </a>
-    </nav>
+    <div v-if="!client">
+        <ul class="pagination pagination-sm m-0 float-left">
+            <p> {{pagination.from}} - {{pagination.to}} of {{pagination.total}} </p>
+        </ul>
+        <ul class="pagination pagination-sm m-0 float-right">
+            <li class="page-item" v-if="pagination.prevPageUrl"><a class="page-link" href="#" @click="$emit('prev');">{{pagination.from}}</a></li>
+            <li class="page-item" v-else :disabled="true"><a class="page-link" href="#">{{pagination.from}}</a></li>
+            <li class="page-item" v-if="pagination.nextPageUrl"><a class="page-link" href="#" @click="$emit('next');">{{pagination.to}}</a></li>
+            <li class="page-item" v-else :disabled="true"><a class="page-link" href="#">{{pagination.to}}</a></li>
+        </ul>
+    </div>
+     <div v-else>
+        <ul class="pagination pagination-sm m-0 float-left">
+            <p> {{pagination.from}} - {{pagination.to}} of {{filtered.length}} </p>
+            <p> Filtered from {{pagination.total}} total entries.</p>
+        </ul>
+        <ul class="pagination pagination-sm m-0 float-right">
+            <li class="page-item" v-if="pagination.prevPage"><a class="page-link" href="#" @click="$emit('prev');">{{pagination.from}}</a></li>
+            <li class="page-item" v-else :disabled="true"><a class="page-link" href="#">{{pagination.from}}</a></li>
+            <li class="page-item" v-if="pagination.nextPage"><a class="page-link" href="#" @click="$emit('next');">{{pagination.to}}</a></li>
+            <li class="page-item" v-else :disabled="true"><a class="page-link" href="#">{{pagination.to}}</a></li>
+        </ul>
+    </div>
 </template>
 
 <script>
