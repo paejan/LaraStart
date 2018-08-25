712a8d740143b36de9efa10a0f09f5d5ab938603
ahinkle <ahinkle10@gmail.com>
2018-08-24 22:48:29 -0500

    Removed the "Logout" button from the menu.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,18 +9,6 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="./img/profile.png" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">
-                    {{ Auth::user()->name }}
-                </a>
-            </div>
-        </div>
-
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -42,12 +30,6 @@
                     <router-link to="/profile" class="nav-link">
                         <i class="nav-icon fas fa-user"></i><p>Profile</p>
                     </router-link>
-                </li>
-
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fa fa-power-off"></i><p>Logout</p>
-                    </a>
                 </li>
             </ul>
         </nav> <!-- /.sidebar-menu -->
