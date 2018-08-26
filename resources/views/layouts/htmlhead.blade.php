44cff9134ba0a4efa786bdee7998413bccbb9847
ahinkle <ahinkle10@gmail.com>
2018-08-25 20:17:23 -0500

    HTML Header

--- resources/views/layouts/htmlhead.blade.php
+++ resources/views/layouts/htmlhead.blade.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>AdminLTE - @yield('htmlheader', 'Title Here') </title>
+    <title>LaraStart - @yield('htmlheader') </title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="{{ url('/css/app.css') }}">
