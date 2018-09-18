affe65994c3338b89877388d82245710381130d8
Andy Hinkle <ahinkle10@gmail.com>
2018-09-17 22:17:52 -0500

    Permissions Seeder

--- database/seeds/PermissionsSeeder.php
+++ database/seeds/PermissionsSeeder.php
@@ -1,16 +1,28 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class PermissionsSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the Permissions and Roles needed in the application.
      *
      * @return void
      */
     public function run()
     {
-        //
+        // Roles
+        $admin = Role::create(['name' => 'Administrator']);
+
+        // Permissions
+        Permission::create(['name' => 'create users']);
+        Permission::create(['name' => 'view users']);
+        Permission::create(['name' => 'edit users']);
+        Permission::create(['name' => 'delete users']);
+
+        // Give Administrator all permissions.
+        $admin->givePermissionTo(Permission::all());
     }
 }
