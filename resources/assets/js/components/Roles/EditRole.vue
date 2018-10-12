b583594d602fbdc2a4ea2a4d7341d96a506fb306
Andy Hinkle <ahinkle10@gmail.com>
2018-10-12 18:32:44 +0200

    Remove Mounted Method

--- resources/assets/js/components/Roles/EditRole.vue
+++ resources/assets/js/components/Roles/EditRole.vue
@@ -53,10 +53,6 @@
 
 <script>
     export default {
-        mounted() {
-            // console.log("Component mounted.");
-        },
-
         created() {
             this.getPermissions();
             this.getRole();
